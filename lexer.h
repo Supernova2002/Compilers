e5bbcbf4654c68600ad5de2a2c3a04dc10c4ecf4
David Stekol <dstekol@gmail.com>
2023-02-13 17:14:29 -0500

    before implementation of enums into lexer

---
+++ lexer.h
@@ -0,0 +1,36 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+
+struct number{
+        enum types {
+                SIGNED_INT,
+                UNSIGNED_INT,
+                SIGNED_LONG,
+                UNSIGNED_LONG,
+                UNSIGNED_LONGLONG,
+                TYPE_DOUBLE,
+                TYPE_FLOAT,
+                LONG_DOUBLE
+
+        };      
+        enum types type;
+        union {
+                int charVal;
+                long int intVal;
+                long double realVal;
+        }value;
+
+
+
+
+};
+struct number number;
+
+typedef union{
+        char *string;
+        struct number number; 
+        
+} YYSTYPE;
+extern YYSTYPE yylval;
+#endif
